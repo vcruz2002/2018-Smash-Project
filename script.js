0f959d999368bc6f6c9b71b13a54c935ca8cc6f7
vcruz2002 <31362121+vcruz2002@users.noreply.github.com>
2018-07-18 13:57:01 -0700

    Add files via upload
    new

--- script.js
+++ script.js
@@ -15,3 +15,42 @@ window.onclick = function(event) {
     }
   }
 }
+
+function myLine(){
+    setInterval();
+    betInterval();
+}
+var startX = 0;
+var startY = 400;
+var endX = 800; //1000
+var endY = 0;
+var amount = 0;
+
+var c = document.getElementById("myGraph");
+var ctx = c.getContext("2d")
+    setInterval (function(){
+        amount += 0.003;
+    ctx.clearRect(startX,startY,endX,endY);
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(startX + (endX -startY) * amount, startY + (endY - startY) * amount);
+    ctx.stroke();
+    }, 30);
+
+var startA = 801;
+var startB = 1;
+var endA = 901;
+var endB = 401;
+    setInterval (function(){
+        amount += 0.003;
+    ctx.clearRect(startA,startB,endA,endB);
+    ctx.moveTo(startA, startB);
+    ctx.lineTo(startA + (endA -startB) * amount, startB + (endB - startB) * amount);
+    ctx.stroke();
+    }, 30);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML ="victor.cruz@scholar.smashprogram.lpfi.org";
+}
+
+  
+
